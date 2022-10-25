504b88741e43defbd0b6cc14fafe09979923e14f
Mary Strodl <ipadlover8322@gmail.com>
2022-10-25 11:30:36 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const {App} = require("@slack/bolt");
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
+const BLOCKED_CHANNEL = process.env.BLOCKED_CHANNELS.split(",");
+
+app.message("@channel", async ({message, client, ack, say}) => {
+  if (BLOCKED_CHANNELS.includes(message.channel.id)) {
+    await client.reactions.add({
+      name: "no_entry",
+      channel: command.channel_id,
+      timestamp: command.ts,
+    });
+    return;
+  }
+  await respond({
+    text: "<!channel>",
+    thread_ts: message.thread_ts || message.ts,
+  });
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+})();
